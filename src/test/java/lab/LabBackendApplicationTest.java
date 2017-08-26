d34236e9968cf8fc58426bc6e492227dbe0fd437
Walison Moreira <walison.moreira@gmail.com>
2017-08-26 11:43:16 -0300

    Atualização dos teste.

--- src/test/java/lab/LabBackendApplicationTest.java
+++ src/test/java/lab/LabBackendApplicationTest.java
@@ -3,37 +3,42 @@ package lab;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import io.restassured.RestAssured;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "/load.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = "/clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Sql(scripts = "/load.sql", executionPhase = BEFORE_TEST_METHOD)
+@Sql(scripts = "/clean.sql", executionPhase = AFTER_TEST_METHOD)
 public class LabBackendApplicationTest {
 
-  @Value("${local.server.port}")
-  private int serverPort;
+  @LocalServerPort
+  private int randomServerPort;
 
   @Before
   public void setUp() throws Exception {
-    RestAssured.port = serverPort;
+    RestAssured.port = randomServerPort;
   }
 
   @Test
   public void vendaTest() {
-    get("/api/vendas/-2").
+    get("/api/vendas/-1").
     then().
-      body("produto", equalTo("Pastel de Carne"));
+      body("produto", equalTo("Pastel de Carne")).
+      body("quantidade", equalTo(4));
   }
 
 }
--- src/test/resources/load.sql
+++ src/test/resources/load.sql
@@ -1,3 +1,1 @@
-INSERT INTO venda (codigo, produto, quantidade) VALUES (-1, 'Coca cola', 2);
-INSERT INTO venda (codigo, produto, quantidade) VALUES (-2, 'Pastel de Carne', 4);
-INSERT INTO venda (codigo, produto, quantidade) VALUES (-3, 'Balinhas', 20);
+INSERT INTO venda (codigo, produto, quantidade) VALUES (-1, 'Pastel de Carne', 4);
